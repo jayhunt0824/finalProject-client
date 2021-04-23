bdc561c40a51ed7100b1acff3e80a30b5c68f380
jen <jayhunt824@gmail.com>
2021-04-23 12:48:41 -0400

    Friday Afternoon: everything working

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import "./App.css";
-// import { Navbar } from "./components/Home/Navbar";
-// import { MainPage } from "./components/Home/MainPage";
-// import { Auth } from "./components/Auth/Auth";
-// import { Route } from "react-router-dom";
-import Routes from "./components/Home/Routes";
+import { Auth } from "./components/Auth/Auth";
 import { SiteBar } from "./components/Home/Navbar";
 import { Footer } from "./components/Home/Footer";
 import MainPage, { MainPageProps } from "./components/Home/MainPage";
-import { BrowserRouter } from "react-router-dom";
 
 export interface AppProps {
   editSearchTerm?: MainPageProps;
@@ -36,13 +31,9 @@ class App extends React.Component<AppProps, AppState> {
     return (
       <div>
         <SiteBar />
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
-        {/* <SiteBar />
+        <Auth />
         <MainPage />
 
-        <Footer /> */}
         <Footer />
       </div>
     );
