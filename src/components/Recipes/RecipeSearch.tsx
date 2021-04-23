287302d50a46d75ea9efc832fa10ee2d67347462
jen <jayhunt824@gmail.com>
2021-04-23 12:08:20 -0400

    built out most of recipeSearch

--- src/components/Recipes/RecipeSearch.tsx
+++ src/components/Recipes/RecipeSearch.tsx
@@ -1,8 +1,18 @@
 import * as React from "react";
+import {Input} from "reactstrap";
+// import {DrinkResponse, Drink} from "./RecipeCardsInterface";
+// import RecipeCards from "./RecipeCards";
 
-export interface RecipeSearchProps {}
 
-export interface RecipeSearchState {}
+export interface RecipeSearchProps {
+    drinks: string;
+}
+
+export interface RecipeSearchState {
+    editSearchTerm: string;
+    drinksURL: string;
+    drinkinformation: Drink[];
+}
 
 class RecipeSearch extends React.Component<
   RecipeSearchProps,
@@ -10,10 +20,44 @@ class RecipeSearch extends React.Component<
 > {
   constructor(props: RecipeSearchProps) {
     super(props);
-    this.state = {};
+    this.state = { drinkinformation: [], drinks: ""
+
+    };
   }
+
+fetchRecipes=(e: React.SyntheticEvent )=>{
+    const drinksURL: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.state.drinks}`;
+    
+    fetch(drinksURL)
+    .then((res)=> res.json())
+    .then((json: DrinkResponse)=>{
+        console.log(json);
+        this.setState({drinkinformation: json.results})
+    })
+}
+
+editSearchTerm = (e: React.SyntheticEvent) => {
+    let search = this.state.drinks;
+    this.setState({ drinks: e.target.value });
+    console.log(search);
+  };
+
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <form>
+          <Input
+            type="text"
+            placeholder="Search Here"
+            value={this.state.drinks}
+            onChange={this.editSearchTerm}
+          />
+          <button type="submit" onSubmit={this.}> Search</button>
+          <h3>Results:</h3>
+        </form>
+      </div>
+    );
   }
 }
 
